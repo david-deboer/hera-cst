1f8a25ea7fadfcd164b5e72410a15b01c04ee674
Aaron Parsons <aparsons@berkeley.edu>
2015-12-10 23:03:47 -0800

    Added freqs ending in 5 to mdl02.

--- scripts/plot_hmap.py
+++ scripts/plot_hmap.py
@@ -31,46 +31,48 @@ opts,args = o.parse_args(sys.argv[1:])
 
 cmap = p.get_cmap(opts.cmap)
 
-map = Basemap(projection='ortho',lat_0=90,lon_0=180,rsphere=1.)
-h = a.map.Map(fromfits=args[0])
-print 'SCHEME:', h.scheme()
-print 'NSIDE:', h.nside()
-if opts.interp: h.set_interpol(True)
+for filename in args:
+    print 'Reading', filename
+    map = Basemap(projection='ortho',lat_0=90,lon_0=180,rsphere=1.)
+    h = a.map.Map(fromfits=filename)
+    print 'SCHEME:', h.scheme()
+    print 'NSIDE:', h.nside()
+    if opts.interp: h.set_interpol(True)
 
-lons,lats,x,y = map.makegrid(360/opts.res,180/opts.res, returnxy=True)
-lons = 360 - lons
-lats *= a.img.deg2rad; lons *= a.img.deg2rad
-y,x,z = a.coord.radec2eq(n.array([lons.flatten(), lats.flatten()]))
-ax,ay,az = a.coord.latlong2xyz(n.array([0,0]))
-#x = x - ax
-#y = y - ay
-#z = z - az
-try: data, indices = h[x,y,z]
-except(ValueError): data = h[x,y,z]
-data.shape = lats.shape
-data /= h[0,0,1]
-#data = data**2 # only if a voltage beam
-data = data_mode(data, opts.mode)
+    lons,lats,x,y = map.makegrid(360/opts.res,180/opts.res, returnxy=True)
+    lons = 360 - lons
+    lats *= a.img.deg2rad; lons *= a.img.deg2rad
+    y,x,z = a.coord.radec2eq(n.array([lons.flatten(), lats.flatten()]))
+    ax,ay,az = a.coord.latlong2xyz(n.array([0,0]))
+    #x = x - ax
+    #y = y - ay
+    #z = z - az
+    try: data, indices = h[x,y,z]
+    except(ValueError): data = h[x,y,z]
+    data.shape = lats.shape
+    data /= h[0,0,1]
+    #data = data**2 # only if a voltage beam
+    data = data_mode(data, opts.mode)
 
 
-map.drawmapboundary()
-map.drawmeridians(n.arange(0, 360, 30))
-map.drawparallels(n.arange(0, 90, 10))
+    map.drawmapboundary()
+    map.drawmeridians(n.arange(0, 360, 30))
+    map.drawparallels(n.arange(0, 90, 10))
 
-if opts.max is None: max = data.max()
-else: max = opts.max
-if opts.drng is None:
-    min = data.min()
-#    if min < (max - 10): min = max-10
-else: min = max - opts.drng
-step = (max - min) / 10
-levels = n.arange(min-step, max+step, step)
-print min,max
-#data = data.clip(min, max)
-#data = n.ma.array(data, mask=mask)
-#min=0
-map.imshow(data, vmax=max, vmin=min, cmap=cmap)
-#map.contourf(cx,cy,data,levels,linewidth=0,cmap=cmap)
-p.colorbar()
+    if opts.max is None: max = data.max()
+    else: max = opts.max
+    if opts.drng is None:
+        min = data.min()
+    #    if min < (max - 10): min = max-10
+    else: min = max - opts.drng
+    step = (max - min) / 10
+    levels = n.arange(min-step, max+step, step)
+    print min,max
+    #data = data.clip(min, max)
+    #data = n.ma.array(data, mask=mask)
+    #min=0
+    map.imshow(data, vmax=max, vmin=min, cmap=cmap)
+    #map.contourf(cx,cy,data,levels,linewidth=0,cmap=cmap)
+    p.colorbar()
 
-p.show()
+    p.show()
